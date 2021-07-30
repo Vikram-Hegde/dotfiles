5fad1854284feae3413645dc9c67f348b85dd2f2
Vikram-Hegde <vikramhegde1440@gmail.com>
2021-07-30 10:06:52 +0530

    Added bin and gitconfig

---
+++ bin/create.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+read -p "Enter Code editor: " editor
+read -p "Enter file name: " fileName
+
+openEditor() {
+    echo "Off you go 🚀"
+    mkdir "$2" && cd "$2" && mkdir styles images
+    cp ~/Templates/Web/index.html ./
+    cd styles && mkdir scss css
+    cd scss && cp ~/Templates/Web/*.scss ./
+    cd ../../
+    "$1" .
+    cd styles && sass --no-source-map --watch scss:css
+}
+
+if [ $editor == "code" ] || [ $editor == "vim" ]
+then
+    openEditor "$editor" "$fileName"
+else
+    echo -e "\nEditor not available 😕. Try code or vim"
+fi
