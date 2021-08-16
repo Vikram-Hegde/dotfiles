745c77383269eaaa58d432d66eb5652ef82a7657
Vikram-Hegde <vikramhegde1440@gmail.com>
2021-08-16 21:11:18 +0530

    some cool features added

---
+++ .config/fish/functions/compile-sass.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function compile-sass --wraps=/home/vikram/bin/compile-sass.sh --description 'alias compile-sass=/home/vikram/bin/compile-sass.sh'
+  /home/vikram/bin/compile-sass.sh  $argv;
+end
