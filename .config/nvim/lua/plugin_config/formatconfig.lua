504c7d476f9024977dcfb8d329b2ad58706d1e32
Vikram-Hegde <vikramhegde1440@gmail.com>
2021-12-13 14:50:19 +0530

    fixed formatting issues

--- .config/nvim/lua/plugin_config/formatconfig.lua
+++ .config/nvim/lua/plugin_config/formatconfig.lua
@@ -1,40 +1,68 @@
-require'format'.setup {
-    ["*"] = {
-        {cmd = {"sed -i 's/[ \t]*$//'"}} -- remove trailing whitespace
-    },
-    vim = {
-        {
-            cmd = {"luafmt -w replace"},
-            start_pattern = "^lua << EOF$",
-            end_pattern = "^EOF$"
-        }
-    },
-    vimwiki = {
-        {
-            cmd = {"prettier -w --parser babel"},
-            start_pattern = "^{{{javascript$",
-            end_pattern = "^}}}$"
-        }
-    },
-    lua = {
-        {
-            cmd = {
-                function(file)
-                    return string.format("luafmt -w --use-tabs -i 2 replace %s", file)
-                end
-            }
-        }
-    },
-    javascript = {
-        {cmd = {"prettier -w", "./node_modules/.bin/eslint --fix"}}
-    },
-    markdown = {
-        {cmd = {"prettier -w"}},
-        {
-            cmd = {"prettier -w"},
-            start_pattern = "^```js$",
-            end_pattern = "^```$",
-            target = "current"
-        }
-    }
-}
+require("formatter").setup(
+	{
+		filetype = {
+			javascript = {
+				-- prettier
+				function()
+					return {
+						exe = "prettier",
+						args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote"},
+						stdin = true
+					}
+				end
+			},
+			scss = {
+				-- prettier
+				function()
+					return {
+						exe = "prettier",
+						args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote"},
+						stdin = true
+					}
+				end
+			},
+			html = {
+				-- prettier
+				function()
+					return {
+						exe = "prettier",
+						args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote"},
+						stdin = true
+					}
+				end
+			},
+			lua = {
+				-- luafmt
+				function()
+					return {
+						exe = "luafmt",
+						args = {"--use-tabs", "--indent-count", 2, "--stdin"},
+						stdin = true
+					}
+				end
+			},
+			cpp = {
+				-- clang-format
+				function()
+					return {
+						exe = "clang-format",
+						args = {"--assume-filename", vim.api.nvim_buf_get_name(0)},
+						stdin = true,
+						cwd = vim.fn.expand("%:p:h") -- Run clang-format in cwd of the file.
+					}
+				end
+			},
+			c = {
+				-- clang-format
+				function()
+					return {
+						exe = "clang-format",
+						args = {"--assume-filename", vim.api.nvim_buf_get_name(0)},
+						stdin = true,
+						cwd = vim.fn.expand("%:p:h") -- Run clang-format in cwd of the file.
+					}
+				end
+			}
+		}
+	}
+)
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,168 +1,197 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	packer_bootstrap =
+		fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
 end
 
-vim.cmd([[
+vim.cmd(
+	[[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerCompile
   augroup end
-]])
+]]
+)
 
-local theme = 'github-nvim-theme'
+local theme = "github-nvim-theme"
 
-return require('packer').startup(function(use)
-  -- Plugin List
-  use { 'wbthomason/packer.nvim' }
+return require("packer").startup(
+	function(use)
+		-- Plugin List
+		use {"wbthomason/packer.nvim"}
 
-	use {
-		'neoclide/coc.nvim',
-		branch = 'release',
-		event = 'BufWinEnter'
-	}
+		use {
+			"neoclide/coc.nvim",
+			branch = "release",
+			event = "BufWinEnter"
+		}
 
-	-- Themes
-	use {
-		'navarasu/onedark.nvim',
-		event = 'UIEnter',
-		-- config = function() vim.g.onedark_style = 'darker' vim.cmd[[ colorscheme onedark ]] end
-	}
+		-- Themes
+		use {
+			"navarasu/onedark.nvim",
+			event = "UIEnter"
+			-- config = function() vim.g.onedark_style = 'darker' vim.cmd[[ colorscheme onedark ]] end
+		}
 
-	use {
-		"projekt0n/github-nvim-theme",
-		event = 'UIEnter',
-		config = function() require('github-theme').setup() end,
-	}
+		use {
+			"projekt0n/github-nvim-theme",
+			event = "UIEnter",
+			config = function()
+				require("github-theme").setup({
+					transparent = 'false',
+					theme_style = 'dark'
+				})
+			end
+		}
 
-	-- use {
-	-- 	'sainnhe/everforest',
-	-- 	event = 'VimEnter',
-	-- 	config = function ()
-	-- 		// TODO
-	-- 	end
-	-- }
+		-- use {
+		-- 	'sainnhe/everforest',
+		-- 	event = 'VimEnter',
+		-- 	config = function ()
+		-- 		// TODO
+		-- 	end
+		-- }
 
-	-- use {
-	-- 	'Pocco81/Catppuccino.nvim',
-	-- 	event = 'VimEnter',
-	-- 	config = function ()
-	-- 		// TODO
-	-- 	end
-	-- }
+		-- use {
+		-- 	'Pocco81/Catppuccino.nvim',
+		-- 	event = 'VimEnter',
+		-- 	config = function ()
+		-- 		// TODO
+		-- 	end
+		-- }
 
-	-- Handy Utils
+		-- Handy Utils
 
-	use {
-			'numToStr/Comment.nvim',
+		use {
+			"numToStr/Comment.nvim",
 			after = theme,
 			config = function()
-					require'Comment'.setup()
+				require "Comment".setup()
 			end
-	}
+		}
 
-	-- use {
-	-- 	'lewis6991/gitsigns.nvim',
-	-- 	requires = {
-	-- 		'nvim-lua/plenary.nvim'
-	-- 	},
-	-- 	tag = 'release', -- To use the latest release
-	-- 	event = 'InsertEnter',
-	-- 	config = function()
-	-- 		require'gitsigns'.setup()
-	-- 	end
-	-- }
-	--
-	use {
-		'norcalli/nvim-colorizer.lua',
-		after = theme,
-		ft = {'html', 'css', 'js', 'sass', 'scss'},
-		cmd = 'ColorizerToggle',
-		config = function() require'colorizer'.setup{} end
-	}
+		-- use {
+		-- 	'lewis6991/gitsigns.nvim',
+		-- 	requires = {
+		-- 		'nvim-lua/plenary.nvim'
+		-- 	},
+		-- 	tag = 'release', -- To use the latest release
+		-- 	event = 'InsertEnter',
+		-- 	config = function()
+		-- 		require'gitsigns'.setup()
+		-- 	end
+		-- }
+		--
+		use {
+			"norcalli/nvim-colorizer.lua",
+			after = theme,
+			ft = {"html", "css", "js", "sass", "scss"},
+			cmd = "ColorizerToggle",
+			config = function()
+				require "colorizer".setup {}
+			end
+		}
 
-	use { 'Pocco81/AutoSave.nvim',
-		event = {'BufWritePost', 'InsertLeave'},
-		config = function() require'plugin_config.autosave' end
-	}
+		use {
+			"Pocco81/AutoSave.nvim",
+			event = {"BufWritePost"},
+			config = function()
+				require "plugin_config.autosave"
+			end
+		}
 
-	use { 'jiangmiao/auto-pairs', event = 'BufWinEnter' }
+		use {"jiangmiao/auto-pairs", event = "BufWinEnter"}
 
-	use {
-		'lukas-reineke/format.nvim',
-		cmd = 'Format',
-		event = 'BufWrite',
-		config = function () require'plugin_config.formatconfig' end,
-	}
+		use {
+			"mhartington/formatter.nvim",
+			cmd = "Format",
+			event = "BufWrite",
+			config = function()
+				require "plugin_config.formatconfig"
+			end
+		}
 
-	use {
-		'blackCauldron7/surround.nvim',
-		event = 'InsertEnter',
-		config = function()
+		use {
+			"blackCauldron7/surround.nvim",
+			event = "InsertEnter",
+			config = function()
 				-- sandwich replaces substitue command (s) in case
 				-- needed change to surround
-			require'surround'.setup {mappings_style = 'sandwich'}
-		end
-	}
+				require "surround".setup {mappings_style = "sandwich"}
+			end
+		}
 
-	use {
-		'akinsho/toggleterm.nvim',
-		event = 'BufWinEnter',
-		config = function()
-			require'plugin_config.toggleterm'
-		end
-	}
+		use {
+			"akinsho/toggleterm.nvim",
+			event = "BufWinEnter",
+			config = function()
+				require "plugin_config.toggleterm"
+			end
+		}
 
-	use {
-		'nvim-telescope/telescope.nvim',
-		requires = 'nvim-lua/plenary.nvim' ,
-		event = 'BufWinEnter',
-		config = function() require'plugin_config.telescope' end
-	}
+		use {
+			"nvim-telescope/telescope.nvim",
+			requires = "nvim-lua/plenary.nvim",
+			event = "BufWinEnter",
+			config = function()
+				require "plugin_config.telescope"
+			end
+		}
 
-	-- Extra stuffs (not important)
-	use {
-			'nvim-treesitter/nvim-treesitter',
-			run = ':TSUpdate',
+		-- Extra stuffs (not important)
+		use {
+			"nvim-treesitter/nvim-treesitter",
+			run = ":TSUpdate",
 			after = theme,
-			config = function () require'plugin_config.treesitter' end
-	}
+			config = function()
+				require "plugin_config.treesitter"
+			end
+		}
 
-	use {
-		"SmiteshP/nvim-gps",
-		after = 'nvim-treesitter',
-		requires = "nvim-treesitter/nvim-treesitter",
-		config = function() require'plugin_config.gps' end
-	}
+		use {
+			"SmiteshP/nvim-gps",
+			after = "nvim-treesitter",
+			requires = "nvim-treesitter/nvim-treesitter",
+			config = function()
+				require "plugin_config.gps"
+			end
+		}
 
-	use {
-		'abecodes/tabout.nvim',
-		config = function() require'plugin_config.tabout' end,
-		wants = 'nvim-treesitter',
-		event = 'InsertEnter'
-	}
+		use {
+			"abecodes/tabout.nvim",
+			config = function()
+				require "plugin_config.tabout"
+			end,
+			wants = "nvim-treesitter",
+			event = "InsertEnter"
+		}
 
-	use {
-		'kyazdani42/nvim-web-devicons',
-		after = theme,
-	}
+		use {
+			"kyazdani42/nvim-web-devicons",
+			after = theme
+		}
 
-	use {
-		'nvim-lualine/lualine.nvim',
-		after = 'nvim-gps',
-		config = function() require'plugin_config.lualine' end
-	}
+		use {
+			"nvim-lualine/lualine.nvim",
+			after = "nvim-gps",
+			config = function()
+				require "plugin_config.lualine"
+			end
+		}
 
-	use {
-		'akinsho/bufferline.nvim',
-		after = 'nvim-web-devicons',
-		config = function() require'plugin_config.bufferline' end
-	}
+		use {
+			"akinsho/bufferline.nvim",
+			after = "nvim-web-devicons",
+			config = function()
+				require "plugin_config.bufferline"
+			end
+		}
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require'packer'.sync()
-  end
-end)
+		-- Automatically set up your configuration after cloning packer.nvim
+		-- Put this at the end after all plugins
+		if packer_bootstrap then
+			require "packer".sync()
+		end
+	end
+)
